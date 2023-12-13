22a2897669f828aa64314874674b38ca8a061118
praveen-79 <150162362+praveen-79@users.noreply.github.com>
2023-12-13 21:40:16 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,79 +1,61 @@
-const request = require("supertest");
-const db = require("../models/index");
-const app = require("../app");
-let server;
-let agent;
+/* eslint-disable no-undef */
+const todoList = require("../todo");
 
-describe("Test case for database", () => {
-  beforeAll(async () => {
-    await db.sequelize.sync({ force: true });
-    server = app.listen(3000, () => {});
-    agent = request.agent(server);
-  });
-  afterAll(async () => {
-    try {
-      await db.sequelize.close();
-      await server.close();
-    } catch (error) {
-      console.log(error);
-    }
-  });
-  test("Creates a todo and responds with json at /todos POST endpoint", async () => {
-    const response = await agent.post("/todos").send({
-      title: "Buy milk",
-      dueDate: new Date().toISOString(),
+const { add, markAsComplete, all, dueToday, dueLater, overdue } = todoList();
+
+describe("Todo test suite", () => {
+  test("should add new todo", () => {
+    expect(all.length).toBe(0);
+    const date = new Date();
+    const yd = new Date(date);
+    const td = new Date(date);
+    td.setDate(date.getDate() + 1);
+    yd.setDate(date.getDate() - 1);
+    add({
+      title: "Todo test",
       completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
     });
-    expect(response.statusCode).toBe(200);
-    expect(response.header["content-type"]).toBe(
-      "application/json; charset=utf-8"
-    );
-    const parsedResponse = JSON.parse(response.text);
-    expect(parsedResponse.id).toBeDefined();
-  });
-  test("Mark todo as a completed", async () => {
-    const res = await agent.post("/todos").send({
-      title: "Do HomeWork",
-      dueDate: new Date().toISOString(),
+    add({
+      title: "Todo test",
       completed: false,
+      dueDate: yd.toLocaleDateString("en-CA"),
     });
-    const parseResponse = JSON.parse(res.text);
-    const todoID = parseResponse.id;
-    expect(parseResponse.completed).toBe(false);
-    const changeTodo = await agent
-      .put(`/todos/${todoID}/markAsCompleted`)
-      .send();
-    const parseUpadteTodo = JSON.parse(changeTodo.text);
-    expect(parseUpadteTodo.completed).toBe(true);
+    add({
+      title: "Todo test",
+      completed: false,
+      dueDate: td.toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(3);
   });
 
-  test("Fetches all todos in the database using /todos endpoint", async () => {
-    await agent.post("/todos").send({
-      title: "Buy xbox",
-      dueDate: new Date().toISOString(),
-      completed: false,
-    });
-    await agent.post("/todos").send({
-      title: "Buy ps3",
-      dueDate: new Date().toISOString(),
-      completed: false,
-    });
-    const response = await agent.get("/todos");
-    const parsedResponse = JSON.parse(response.text);
-    expect(parsedResponse.length).toBe(4);
-    expect(parsedResponse[3]["title"]).toBe("Buy ps3");
+  test("should mark a todo as complete", () => {
+    // Ensure the todo is initially marked as incomplete
+    expect(all[0].completed).toBe(false);
+
+    // Mark the todo as complete
+    markAsComplete(0);
+
+    // Ensure the todo is now marked as complete
+    expect(all[0].completed).toBe(true);
   });
 
-  test("Deletes a todo with the given ID if it exists and sends a boolean response", async () => {
-    const response = await agent.post("/todos").send({
-      title: "Buy xbox",
-      dueDate: new Date().toISOString(),
-      completed: false,
-    });
-    const parsedResponse = JSON.parse(response.text);
-    const todoID = parsedResponse.id;
-    const res = await agent.delete(`/todos/${todoID}`).send();
-    const bool = Boolean(res.text);
-    expect(bool).toBe(true);
+  test("should retrive a todo as duetoday", () => {
+    expect(all.length).toBe(3);
+    const k = dueToday();
+    console.log(k);
+    expect(k.length).toBe(1);
+  });
+  test("should retrive a todo as overdue", () => {
+    let k = [];
+    expect(k.length).toBe(0);
+    k = overdue();
+    expect(k.length).toBe(1);
+  });
+  test("should retrive a todo as laterdue", () => {
+    let k = [];
+    expect(k.length).toBe(0);
+    k = dueLater();
+    expect(k.length).toBe(1);
   });
 });
