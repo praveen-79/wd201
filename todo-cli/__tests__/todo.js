78fba6a9f7723b2a31604ba613cf807fa65694ad
praveen-79 <150162362+praveen-79@users.noreply.github.com>
2023-12-21 17:43:06 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,16 +1,8 @@
 const todoList = require("../todo");
 
+const { add, markAsComplete, all, dueToday, dueLater, overdue } = todoList();
+
 describe("Todo test suite", () => {
-  let { add, markAsComplete, all, dueToday, dueLater, overdue } = {};
-
-  beforeEach(() => {
-    ({ add, markAsComplete, all, dueToday, dueLater, overdue } = todoList());
-  });
-
-  afterEach(() => {
-    // Teardown, if needed
-  });
-
   test("should add new todo", () => {
     expect(all.length).toBe(0);
     const date = new Date();
@@ -18,7 +10,6 @@ describe("Todo test suite", () => {
     const td = new Date(date);
     td.setDate(date.getDate() + 1);
     yd.setDate(date.getDate() - 1);
-
     add({
       title: "Todo test",
       completed: false,
@@ -34,36 +25,32 @@ describe("Todo test suite", () => {
       completed: false,
       dueDate: td.toLocaleDateString("en-CA"),
     });
-
     expect(all.length).toBe(3);
   });
 
   test("should mark a todo as complete", () => {
-    add({
-      title: "Todo to be completed",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
-
+    
     expect(all[0].completed).toBe(false);
-    markAsComplete(0);
+
+     markAsComplete(0);
+
+   
     expect(all[0].completed).toBe(true);
   });
 
-  test("should retrieve a todo as due today", () => {
+  test("should retrive a todo as duetoday", () => {
     expect(all.length).toBe(3);
     const k = dueToday();
+    console.log(k);
     expect(k.length).toBe(1);
   });
-
-  test("should retrieve a todo as overdue", () => {
+  test("should retrive a todo as overdue", () => {
     let k = [];
     expect(k.length).toBe(0);
     k = overdue();
     expect(k.length).toBe(1);
   });
-
-  test("should retrieve a todo as later due", () => {
+  test("should retrive a todo as laterdue", () => {
     let k = [];
     expect(k.length).toBe(0);
     k = dueLater();
